703ab13215c1b7e5511f11714bbfa54cc5d445c8
Jason <unkwn22@naver.com>
2024-08-17 12:00:24 +0900

    admin module init
    1. admin module init create
    2. server port todo

---
+++ admin-api-module/src/main/kotlin/com/example/adminmyvopiserver/AdminMyvopiServerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.adminmyvopiserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdminMyvopiServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<AdminMyvopiServerApplication>(*args)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ rootProject.name = "myvopi-server"
 
 include(
     "user-api-module",
+    "admin-api-module",
     "entity-core-module",
     "common-core-module",
     "auth-core-module",
