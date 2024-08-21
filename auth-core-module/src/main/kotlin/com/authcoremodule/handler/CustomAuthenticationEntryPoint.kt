5e08d627b909b569d39af08959b8e9308d42e451
Jason <unkwn22@naver.com>
2024-08-21 12:36:01 +0900

    existing project change
    - removed email configuration for admin
    - refactored infrastructure alias to common directory
    - created new customaccess denied handler for unauthorized
    - added exception handler for requestmatchers
    - added status column for internalusercommand
    - changed video search to get mapping

---
+++ auth-core-module/src/main/kotlin/com/authcoremodule/handler/CustomAuthenticationEntryPoint.kt
@@ -0,0 +1,29 @@
+package com.authcoremodule.handler
+
+import com.commoncoremodule.exception.ErrorCode
+import com.commoncoremodule.response.CommonResponse
+import com.google.gson.Gson
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.web.AuthenticationEntryPoint
+import org.springframework.stereotype.Component
+
+@Component
+class CustomAuthenticationEntryPoint: AuthenticationEntryPoint {
+
+    override fun commence(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        authException: AuthenticationException?
+    ) {
+        response.status = HttpServletResponse.SC_NOT_FOUND
+        response.contentType = "application/json; charset=UTF-8"
+
+        val exceptionResponse = CommonResponse.fail(
+            message = ErrorCode.PAGE_NOT_FOUND.engErrorMsg,
+            errorCode = ErrorCode.PAGE_NOT_FOUND
+        )
+        response.writer.write(Gson().toJson(exceptionResponse))
+    }
+}
